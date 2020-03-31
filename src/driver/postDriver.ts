f1913823af8ecc52db3ae98feea68992f43a3da1
migitera <migiterayu@crunchtimer.jp>
2020-03-31 16:22:43 +0900

    add: postdriver

---
+++ src/driver/postDriver.ts
@@ -0,0 +1,14 @@
+import { initAxios } from './axios';
+import PostDriver, { PostsJson } from '../interface/driver/postDriver';
+
+export default class PostDriverImpl implements PostDriver {
+  async findAll(): Promise<PostsJson> {
+    try {
+      return await initAxios().get(
+        'user_timeline.json?screen_name=katsuyatakasu'
+      );
+    } catch (e) {
+      return { errors: e.response.data.errors };
+    }
+  }
+}
