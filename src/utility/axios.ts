2a519f86ae1c63bccb940af82ccfd7095e0ad908
nakajimaibuki <nakajimai@crunchtimer.jp>
2020-03-31 23:19:23 +0900

    refs: 不要な記述の削除

--- src/utility/axios.ts
+++ src/utility/axios.ts
@@ -1,5 +1,5 @@
 import axiosBase, { AxiosInstance, AxiosResponse } from 'axios';
-import { apiKey, apiUrl } from '../config.dev';
+import { apiKey } from '../config.dev';
 
 export type LoginError = {
   status?: number;
